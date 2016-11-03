82f718918409fd426251983dcf412819f9be1745
balkh <balkhaev@phystech.edu>
2016-11-03 14:21:17 +0300

    according SOLID

---
+++ src/main/java/ru/sbt/bit/ood/hw1/TradeParserCSV.java
@@ -0,0 +1,38 @@
+package ru.sbt.bit.ood.hw1;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by balkh on 03.11.2016.
+ */
+public class TradeParserCSV implements TradeParser {
+    public Iterable<Trade> parse(String filename) {
+        try {
+            Reader in = new FileReader(filename);
+            Iterable<Trade> records = FromCSVToTrade(CSVFormat.EXCEL.parse(in));
+            return records;
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException("There was an error while parsing CSV file");
+        }
+    }
+
+    private Iterable<Trade> FromCSVToTrade(Iterable<CSVRecord> recordsCSV) {
+        // from CSV to Trade
+        List<Trade> records = new ArrayList<Trade>();
+        for (CSVRecord record : recordsCSV) {
+            Trade trade = new Trade(record.toMap());
+            records.add(trade);
+        }
+        return records;
+    }
+
+}
--- src/main/java/ru/sbt/bit/ood/hw1/TradesJob.java
+++ src/main/java/ru/sbt/bit/ood/hw1/TradesJob.java
@@ -1,57 +1,26 @@
 package ru.sbt.bit.ood.hw1;
 
-import org.apache.commons.csv.CSVFormat;
-import org.apache.commons.csv.CSVRecord;
-import org.apache.commons.net.ftp.FTPClient;
-
-import java.io.*;
-
 public class TradesJob {
 
     private final TradesDAO tradesDAO;
-
-    public TradesJob(TradesDAO tradesDAO) {
+    private final TradeDownloader tradeDownloader;
+    private final TradeParser tradeParser;
+    public TradesJob(TradesDAO tradesDAO, TradeDownloader tradeDownloader, TradeParser tradeParser) {
         this.tradesDAO = tradesDAO;
+        this.tradeDownloader = tradeDownloader;
+        this.tradeParser = tradeParser;
     }
 
     public void run() {
-        String filename = downloadTradesFileFromFTP();
-        Iterable<CSVRecord> tradeRecords = parse(filename);
-        updateTrades(tradeRecords);
+        String filename = tradeDownloader.downloadFile();
+        Iterable<Trade> trades = tradeParser.parse(filename);
+        updateTrades(trades);
     }
 
-    public String downloadTradesFileFromFTP() {
-        FTPClient ftpClient = new FTPClient();
-        try {
-            ftpClient.connect("localhost", 8090);
-            ftpClient.login("foo", "password");
-            File tempFile = File.createTempFile("trades", "download");
-            OutputStream out = new FileOutputStream(tempFile);
-            ftpClient.retrieveFile("public/prod/trades.csv", out);
-            out.close();
-            return tempFile.getAbsolutePath();
-        } catch (IOException e) {
-            e.printStackTrace();
-            throw new RuntimeException("Could not download the file");
-        }
-    }
-
-    private void updateTrades(Iterable<CSVRecord> trades) {
+    private void updateTrades(Iterable<Trade> trades) {
         tradesDAO.deleteAll();
-        for (CSVRecord tradeRecord : trades) {
-            Trade trade = new Trade(tradeRecord.toMap());
+        for (Trade trade : trades) {
             tradesDAO.save(trade);
-        }
-    }
-
-    private Iterable<CSVRecord> parse(String filename) {
-        try {
-            Reader in = new FileReader(filename);
-            Iterable<CSVRecord> records = CSVFormat.EXCEL.parse(in);
-            return records;
-        } catch (IOException e) {
-            e.printStackTrace();
-            throw new RuntimeException("There was an error while parsing CSV file");
         }
     }
 }
